f337956d831dbc471f8b27c193f1a8e9d3114cb6
santi <goez1247@gmail.com>
2025-09-24 18:48:30 -0500

    Solucion de problema con DTO elimar producto

---
+++ DTOs/Product/DeleteProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.DTOs.Product
+{
+    public class DeleteProductDTO
+    {
+        public int ProductID { get; set; }
+
+        public DeleteProductDTO(int productId)
+        {
+            ProductID = productId;
+        }
+    }
+
+}
